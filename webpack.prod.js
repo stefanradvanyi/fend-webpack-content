f0650869ee46edf70a08ffd98e01baec1b70ebed
Stefan Radvanyi <sradvanyi@10.0.0.3>
2020-07-26 17:01:20 +0200

    Add babel

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
+    mode: 'production',
     module: {
         rules: [
             {
@@ -11,5 +13,11 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
